7c79f07fa6153ea20809a9c1014e2ff257508f18
chenshun131 <1539831174@qq.com>
2018-05-20 23:41:59 +0800

    commit

--- eureka/zipkin-server/src/main/java/com/chenshun/zipkinserver/ZipkinServerApplication.java
+++ eureka/zipkin-server/src/main/java/com/chenshun/zipkinserver/ZipkinServerApplication.java
@@ -3,11 +3,12 @@ package com.chenshun.zipkinserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
     }
+
 }
