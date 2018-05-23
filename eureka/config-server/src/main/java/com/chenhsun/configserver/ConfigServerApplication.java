989a30c8385341b303682e7093b55ae6ee925664
chenshun131 <1539831174@qq.com>
2018-05-23 18:06:59 +0800

    commit

--- eureka/config-server/src/main/java/com/chenhsun/configserver/ConfigServerApplication.java
+++ eureka/config-server/src/main/java/com/chenhsun/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.chenhsun.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableConfigServer
