53b3f91640957c7d45bdd9d703651ea75a360392
chenshun131 <1539831174@qq.com>
2018-05-31 08:45:18 +0800

    commit

---
+++ SpringBootDemo19/src/main/java/com/chenshun/springbootdemo19/SpringBootDemo19Application.java
@@ -0,0 +1,13 @@
+package com.chenshun.springbootdemo19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo19Application.class, args);
+    }
+
+}
